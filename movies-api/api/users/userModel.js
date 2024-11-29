9457f4b4129e60b4cbdb7e71de12fed162707dd6
robert-walshh <20102479@mail.wit.ie>
2024-11-29 18:02:24 +0000

    Initialise Dev Env - Data

---
+++ movies-api/api/users/userModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true},
+  password: {type: String, required: true }
+});
+
+const passwordValidator = (password) => {
+  const regularExpression = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return regularExpression.test(password);
+}
+UserSchema.path("password").validate(passwordValidator);
+
+export default mongoose.model('User', UserSchema);
