69006266ae0d4cdcffde47198b36f194f93442d0
Robert Walsh <20102479@mail.wit.ie>
2025-08-26 21:40:55 +0100

    Authentication lab - exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -4,6 +4,9 @@ import express from 'express';
 import {
   getUpcomingMovies
 } from '../tmdb-api';
+import {
+    getMovieGenres
+  } from '../tmdb-api';
 
 const router = express.Router();
 
@@ -42,6 +45,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
+}));
+
+router.get('/tmdb/genres', asyncHandler(async (req, res) => {
+    const movieGenres = await getMovieGenres();
+    res.status(200).json(movieGenres);
 }));
 
 export default router;
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -15,3 +15,21 @@ export const getUpcomingMovies = async () => {
         throw error;
     }
 };
+
+export const getMovieGenres = () => {
+    return fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+      process.env.TMDB_KEY +
+        "&language=en-US"
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
